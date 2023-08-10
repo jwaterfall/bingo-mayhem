44b26aafe3e58278729331430b65363dbfb94d1d
Jack Waterfall <jack.a.waterfall@gmail.com>
2023-08-10 16:55:13 +0100

    feat: inital commit

---
+++ src/main/java/net/jwaterfall/bingomayhem/BingoMayhemModClient.java
@@ -0,0 +1,10 @@
+package net.jwaterfall.bingomayhem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BingoMayhemModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
